8a9064c2231fc3e27294642b40b1b4ab138a1737
Aditya Sinha <31968928+jrsinha2@users.noreply.github.com>
2018-08-16 10:45:39 +0530

    Update sqrt.c

--- sqrt.c
+++ sqrt.c
@@ -5,20 +5,23 @@
 void* func(void *arg)
 {	double *arr;
 	sleep(1);
+	int i;
 	arr=(double*)malloc(100*sizeof(double));
-	for(int i=0;i<=99;i++)
-	{	arr[i]=(double)sqrt((double)i);
+	for(i=0;i<=99;i++)
+	{	arr[i]=sqrt((double)i);
 	}
 	return (void*)arr;
 }
 int main()
 {	pthread_t t;
+	void* ar;
 	double* arr;
-	
+	int i;
 	pthread_create(&t,NULL,func,NULL);
 	printf("Hello\n");
-	pthread_join(t,&arr);
-	for(int i=0;i<100;i++)
+	pthread_join(t,&ar);
+	arr=(double*)ar;
+	for(i=0;i<100;i++)
 		printf("%lf\n",arr[i]);
 	printf("\n");
 	return 0;
