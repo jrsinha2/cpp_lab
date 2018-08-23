431a46794b869d6d2948226f7b98d659064e2ad3
Aditya Sinha <31968928+jrsinha2@users.noreply.github.com>
2018-08-23 11:55:39 +0530

    Add files via upload

---
+++ 1/five.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>			//posix threads
+void *thread(void *arg)
+{	sleep(1);
+	int i=(int)arg;
+	printf("Thread %d\n",i);
+	return NULL;
+}
+int main()
+{	int n=5;
+	for(int i=0;i<5;i++)
+	{	pthread_t t;
+		pthread_create(&t,NULL,thread,i);
+		pthread_join(t,NULL);
+	}
+	return 0;
+}
